da338465cc833953f973253e2e36377db7e6e42a
unknown <giles@roadnight.name>
2019-10-20 16:49:19 +0100

    Display version in title

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { CineworldService } from 'src/app/services/cineworld.service';
+import { version } from '../../../../package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+    constructor() {
+        document.title = `Cineworld Planner ${version}`;
+    }
 
 }
