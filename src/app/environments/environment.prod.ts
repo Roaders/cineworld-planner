3f59f966482dc4c2d6b29c83fd13d15adcff0f34
Giles Roadnight <giles@roadnight.name>
2019-10-21 06:11:13 +0100

    Fix staging url, scrolling

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,10 @@
 import { IEnvironment } from 'src/contracts/contracts';
 
+const baseUrl = window.location.host.indexOf('staging') >= 0 ?
+    'https://cineworld-staging.herokuapp.com' :
+    'https://cineworld-planner.herokuapp.com';
+
 export const environment: IEnvironment = {
     production: true,
-    baseUrl: 'https://cineworld-planner.herokuapp.com'
+    baseUrl
 };
