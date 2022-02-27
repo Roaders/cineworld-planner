51d16f052b2d5a37ec3ad4f252b2eed74cbf84bc
Giles Roadnight <giles.roadnight@gmail.com>
2022-02-27 10:50:07 +0000

    Remove pointless / buggy angular tests

--- src/app/components/date-selector/date-selector.component.spec.ts
+++ src/app/components/date-selector/date-selector.component.spec.ts
@@ -1,48 +1,11 @@
 import { DateSelectorComponent } from './date-selector.component';
 import * as momentImport from 'moment';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { CinemaListComponent } from '../cinema-list/cinema-list.component';
-import { CinemaComponent } from '../cinema/cinema.component';
-import { CinemaHeaderComponent } from '../cinema-header/cinema-header.component';
-import { EventListComponent } from '../event-list/event-list.component';
-import { ItineraryListComponent } from '../itinerary-list/itinerary-list.component';
-import { AttributeSelectorComponent } from '../attribute-selector/attribute-selector.component';
-import { CineworldService } from 'src/app/services/cineworld.service';
-import { CinemaHelper } from 'src/app/helper/cinema-helper';
-import { PreferencesService } from 'src/app/services/preferences.service';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { ItineraryComponent } from '../itinerary/itinerary.component';
 
 describe('DateSelectorComponent', () => {
-    let component: DateSelectorComponent;
-    let fixture: ComponentFixture<DateSelectorComponent>;
 
-    beforeEach(waitForAsync(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                CinemaListComponent,
-                DateSelectorComponent,
-                CinemaComponent,
-                CinemaHeaderComponent,
-                EventListComponent,
-                ItineraryComponent,
-                ItineraryListComponent,
-                AttributeSelectorComponent,
-            ],
-            providers: [ CineworldService, CinemaHelper, PreferencesService ],
-            imports: [ HttpClientModule, AppRoutingModule, FormsModule ]
-        })
-            .compileComponents();
-    }));
-
-    beforeEach(() => {
-        fixture = TestBed.createComponent(DateSelectorComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
-
+    function createInstance(){
+        return new DateSelectorComponent({snapshot: {params: {}}} as any, {} as any);
+    }
 
     const originalMoment = {...momentImport};
 
@@ -59,10 +22,14 @@ describe('DateSelectorComponent', () => {
     });
 
     it('should create', () => {
-        expect(component).toBeDefined();
+        expect(createInstance()).toBeDefined();
     });
 
     it('should return correct list of days', () => {
+
+        const instance = createInstance();
+
+        instance.ngOnInit();
 
         const expectedDays = [
             {description: 'Today', date: '2019-01-27'},
@@ -74,6 +41,6 @@ describe('DateSelectorComponent', () => {
             {description: 'Sat', date: '2019-02-02'},
         ];
 
-        expect(component.days).toEqual(expectedDays);
+        expect(instance.days).toEqual(expectedDays);
     });
 });
