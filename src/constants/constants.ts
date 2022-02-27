873d1eb4156a148aacb5339d18b702affd8509a9
Giles Roadnight <giles.roadnight@gmail.com>
2022-02-27 10:19:40 +0000

    Setup secure server

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,5 +3,5 @@ import { IUrlLookup } from 'src/contracts/contracts';
 export const urlLookup: IUrlLookup = {
     local: 'http://localhost:3000',
     ring: 'http://ring:43000',
-    prod: 'http://kingshill.pricklythistle.co.uk:43000'
+    prod: 'https://api.kingshill.cineworld-planner.co.uk:43000'
 };
